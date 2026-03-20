e7e69a65ee8c21368da37037a1497abf0e18135a
Valeev Azamat <vazamat@gmail.com>
2026-03-21 00:38:00 +0300

    init new django project

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include('managementsystem.urls',namespace='managementsystem')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.AdminSite.site_header = 'Моя админка'
+admin.AdminSite.index_title = 'Главная страница настройки администрования приложения'
