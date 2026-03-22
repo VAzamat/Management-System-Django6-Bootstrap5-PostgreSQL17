dec3eea355f5eaaf6010f28b7fba29fc3877b6c7
Valeev Azamat <vazamat@gmail.com>
2026-03-23 01:43:08 +0300

    randomize the GymActivity output

--- managementsystem/views.py
+++ managementsystem/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Banner, GymActivity
+import random
 
 # Create your views here.
 def home(request):
     banners = Banner.objects.filter(is_active=True)
     gymactivities = GymActivity.objects.filter(is_active=True)
+    gymactivities_random = list(gymactivities)
+    random.shuffle(gymactivities_random)
     return render(request,
                   'managementsystem/main.html',
-                  {'banners': banners, 'gymactivities':gymactivities}
+                  {'banners': banners, 'gymactivities':gymactivities, 'gymactivities_random': gymactivities_random}
                   )
