a242e2fe94b99f97cb49f0f351e6b78c44df8fb7
Valeev Azamat <vazamat@gmail.com>
2026-03-23 00:20:13 +0300

    dynamical gymactivities in main.html in rounded-circle

--- managementsystem/models.py
+++ managementsystem/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 
+Nullable = {"null": True, "blank": True}
+
 class Banner(models.Model):
     id = models.UUIDField(
         primary_key=True,
@@ -13,9 +15,8 @@ class Banner(models.Model):
         verbose_name="Название баннера"
     )
     slogan = models.TextField(
-        blank=True,
-        null=True,
-        verbose_name="Слоган баннера"
+        verbose_name="Слоган баннера",
+        **Nullable
     )
     button_text = models.CharField(
         max_length=100,
@@ -37,6 +38,53 @@ class Banner(models.Model):
     class Meta:
         verbose_name = "Баннер"
         verbose_name_plural = "Баннеры"
+
+    def __str__(self):
+        return self.title
+
+
+class GymActivity(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        verbose_name="Уникальный ID"
+    )
+    title = models.CharField(
+        max_length=60,
+        verbose_name="Название",
+    )
+    slogan = models.TextField(
+        verbose_name="Слоган баннера",
+        **Nullable
+    )
+    subtitle = models.CharField(
+        max_length=60,
+        verbose_name="Название подсекции",
+    )
+    description = models.TextField(
+        max_length=250,
+        verbose_name="Описание",
+        **Nullable
+    )
+    image = models.ImageField(
+        upload_to='GymActivities/',
+        verbose_name="Изображение",
+        **Nullable
+    )
+    link_url = models.URLField(
+        max_length=500,
+        verbose_name="Ссылка на страницу",
+        **Nullable
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name="Видимость"
+    )
+
+    class Meta:
+        verbose_name = "Активность в зале"
+        verbose_name_plural = "Активности в зале"
 
     def __str__(self):
         return self.title
--- managementsystem/views.py
+++ managementsystem/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import Banner
+from .models import Banner, GymActivity
 
 # Create your views here.
 def home(request):
     banners = Banner.objects.filter(is_active=True)
-    return render(request, 'managementsystem/main.html', {'banners': banners})
+    gymactivities = GymActivity.objects.filter(is_active=True)
+    return render(request, 'managementsystem/main.html',
+                  {'banners': banners, 'gymactivities':gymactivities})
