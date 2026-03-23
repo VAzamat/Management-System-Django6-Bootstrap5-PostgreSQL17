c5e1ea6fa9d3e0d750a4c4642f3746d59cdd1f9e
Valeev Azamat <vazamat@gmail.com>
2026-03-24 01:44:04 +0300

    sort by created date and well formatted created date at admin panel

--- managementsystem/admin.py
+++ managementsystem/admin.py
@@ -18,11 +18,23 @@ class GymActivityAdmin(admin.ModelAdmin):
 
 @admin.register(PhotoAlbum)
 class PhotoAlbumAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image_tag', 'is_active')
+    list_display = ('title', 'image_tag', 'is_active', 'formatted_created_at')
     list_filter = ('is_active',)
     readonly_fields = ('image_tag',)
+    ordering = ('created_at',)
+
+    #для форматирования даты
+    @admin.display(description='Дата загрузки', ordering='created_at')
+    def formatted_created_at(self, obj):
+        return obj.created_at.strftime("%d.%m.%Y %H:%M")
 
 @admin.register(ImageGallery)
 class ImageGalleryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image_tag', 'is_active', 'album')
+    list_display = ('title', 'image_tag', 'is_active', 'album', 'formatted_created_at')
     list_filter = ('is_active',)
+    ordering = ('album', 'order', 'created_at')
+
+    #для форматирования даты
+    @admin.display(description='Дата загрузки', ordering='created_at')
+    def formatted_created_at(self, obj):
+        return obj.created_at.strftime("%d.%m.%Y %H:%M")
